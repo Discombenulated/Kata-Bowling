2374f05112c86fb656a367919c0ba522b27cab25
Ben Noble <bennoble@Benjamins-MacBook-Pro.local>
2022-10-19 12:00:16 +0100

    Refactored for simpler methods

--- Bowling/Model/Frame.cs
+++ Bowling/Model/Frame.cs
@@ -10,14 +10,33 @@ public class Frame
 
     public int Score()
     {
-        if (Bowled.Equals("X") || Bowled.EndsWith("/")){
+        if (IsStrike() || IsSpare())
+        {
             return 10;
         }
 
-        int firstBowl = Int32.Parse(Bowled[0].ToString());
-        int secondBowl = Int32.Parse(Bowled[1].ToString());
-        int score = firstBowl + secondBowl;
+        int score = GetFirstBowl() + GetSecondBowl();
 
         return score;
+    }
+
+    private bool IsStrike()
+    {
+        return Bowled.Equals("X");
+    }
+
+    private bool IsSpare()
+    {
+        return Bowled.EndsWith("/");
+    }
+
+    private int GetFirstBowl()
+    {
+        return Int32.Parse(Bowled[0].ToString());
+    }
+
+    private int GetSecondBowl()
+    {
+        return Int32.Parse(Bowled[1].ToString());
     }
 }
